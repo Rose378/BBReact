c053499149d6f1c4c544e065511021e8ace401f8
Rose378 <rosemary.katikala7@gmail.com>
2024-04-02 16:51:14 +0530

    BB App Commit

---
+++ backened/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+dotenv.config({path:'./config/config.env'});
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+app.use(cors())
+
+// Express 4.0
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+app.get('/api' , (req,res) => {
+    res.send('welcome to server')
+});
+
+
+
+mongoose.connect(process.env.MONGODB_CLOUD_URL).then(() => {
+    console.log('Mongo DB Connected')
+}).catch((err) => {
+    console.error(err);
+    Process.exit(1);
+})
+
+//configure thw router
+app.use('/api' , require('./functions/apirouter'));
+
+app.listen(port,host,() => {
+    console.log(`server started http://${host}:${port}`)
+})
