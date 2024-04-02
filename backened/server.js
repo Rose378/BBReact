a60ed139fe81eb75ddb2ed52dfa1cbaf34d788df
Rose378 <rosemary.katikala7@gmail.com>
2024-04-02 19:23:44 +0530

    BB App Committ render crs

--- backened/server.js
+++ backened/server.js
@@ -11,6 +11,21 @@ const port = process.env.PORT;
 
 app.use(cors())
 
+// Define the origins from which requests are allowed
+// const allowedOrigins = ['localhost:3001/', 'https://bbreact.onrender.com'];
+
+// Configure CORS middleware
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     // Check if the request origin is included in the allowed origins
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
+
 // Express 4.0
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
